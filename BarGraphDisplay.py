cb46ee20eb9727365b6e0c1da5312ca7b9497151
itdependz <93829749+itdependz@users.noreply.github.com>
2024-02-22 22:57:15 -0500

    dropdown menu for bargraph finished

--- BarGraphDisplay.py
+++ BarGraphDisplay.py
@@ -4,12 +4,12 @@ import streamlit.components.v1 as components
 import streamlit as st
 import plotly.express as px
 
-def BarGraphDisplay(df, list):
+def BarGraphDisplay(df, foodList, targetMetric):
     # Get the corresponding cell to the item in the list under Emissions PEr kilogram
     ylist = []
-    for item in list:
-        num = (float(df[df['Entity'] == item]['Emissions per kilogram'].values[0]))
+    for item in foodList:
+        num = (float(df[df['Entity'] == item][targetMetric].values[0]))
         ylist.append(num)
-    fig = px.bar(df, x=list, y=ylist, title='Food Footprint')
+    fig = px.bar(df, x=foodList, y=ylist, title='Food Footprint', labels={'x': 'Food', 'y':targetMetric})
     
     return fig
--- app.py
+++ app.py
@@ -3,39 +3,61 @@ import pandas as pd
 import streamlit.components.v1 as components
 import streamlit as st
 import plotly.express as px
+import csv
 import BarGraphDisplay
+
+reader = csv.reader(open('./data/food-footprints.csv'))
 
 # Configure the Streamlit Page
 st.set_page_config(
-    page_title="PygWalker",
+    page_title="Enviromental Footprints of Food",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
 # Add a title
-st.title("PygWalker")
+st.title("Enviromental Footprints of Food")
+
 
 # import your Data
 df = pd.read_csv("./data/food-footprints.csv")
 
-# Make the Entity column as a list
-entities = df['Entity'].tolist()
-
+# Setup the Pygwalker
 # Generate the HTML using Pygwalker
 pyg_html = pyg.walk(df, return_html=True)
-
-#create bar chart using plotly
-fig = px.bar(df, x='Entity', y='Emissions per kilogram', title='Food Footprint')
 
 # Embed the generated HTML into the Streamlit App
 components.html(pyg_html, width=1000, height=800, scrolling=True)
 
+
+# Make the Entity column as a list
+entities = df['Entity'].tolist()
+
+
+# Make a metric list
+metrics = next(reader)
+
+for i in metrics:
+    if i == "Entity":
+        metrics.remove(i)
+        break
+
+
+#create bar chart using plotly
+fig = px.bar(df, x='Entity', y='Emissions per kilogram', title='Food Footprint')
+
+
 #embed the plotly chart into the Streamlit App
 st.plotly_chart(fig, use_container_width=True)
 
-options = st.multiselect(
+metricOption = st.selectbox(
+    'Choose a Metric to Display',
+    metrics
+)
+
+foodOptions = st.multiselect(
     'Choose Foods You Want to Display',
     entities
     )
 # Call the BarGraphDisplay function
-st.plotly_chart(BarGraphDisplay.BarGraphDisplay(df, options))
+st.plotly_chart(BarGraphDisplay.BarGraphDisplay(df, foodOptions, metricOption))
